aadc68025474311d7effd79078c5dc6255e472e5
cwhall <53900619+cwhall@users.noreply.github.com>
2020-08-27 16:08:10 -0500

    Add files via upload

---
+++ MSI-Install.ps1
@@ -0,0 +1,12 @@
+$Folder = Read-Host -Prompt "Folder where the msi is stored?"
+$MSI = Read-Host -Prompt "File name of the msi?"
+#Provide the filepath to a csv file you have prepared with the names of computers to install software on
+$Computers = Read-Host -Prompt "File path to .csv file with list of computers?"
+$FullPath = Join-Path -Path $Folder -ChildPath $MSI 
+
+Import-Csv $Computers | ForEach-Object {
+    Copy-Item -Path $FullPath -Destination (Join-Path -Path "\\" -ChildPath $($_.Name) | Join-Path -ChildPath "C$\Windows\Temp" | Join-Path -ChildPath $MSI)
+    Invoke-Command -ComputerName $($_.Name) -ArgumentList $MSI -ScriptBlock {
+        Start-Process -FilePath (Join-Path -Path "C:" -ChildPath "Windows" | Join-Path -ChildPath "Temp" | Join-Path -ChildPath $using:MSI) -Wait -argumentlist '/quiet' 
+    }
+}
