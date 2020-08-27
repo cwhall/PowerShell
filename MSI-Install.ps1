25ab3c84eba5ca69da280b55fa4d65595ec57c2a
cwhall <53900619+cwhall@users.noreply.github.com>
2020-08-27 17:20:27 -0500

    Update MSI-Install.ps1

--- MSI-Install.ps1
+++ MSI-Install.ps1
@@ -1,11 +1,12 @@
 $Folder = Read-Host -Prompt "Folder where the msi is stored?"
 $MSI = Read-Host -Prompt "File name of the msi?"
 $Computers = Read-Host -Prompt "File path to .csv file with list of computers?"
-$FullPath = Join-Path -Path $Folder -ChildPath $MSI 
+$NetworkSharePath = Join-Path -Path $Folder -ChildPath $MSI 
+$ImportedPath = Join-Path -Path "C:\Windows\Temp" -ChildPath $MSI
 
 Import-Csv $Computers | ForEach-Object {
-    Copy-Item -Path $FullPath -Destination (Join-Path -Path "\\" -ChildPath $($_.Name) | Join-Path -ChildPath "C$\Windows\Temp" | Join-Path -ChildPath $MSI)
+    Copy-Item -Path $NetworkSharePath -Destination (Join-Path -Path "\\" -ChildPath $($_.Name) | Join-Path -ChildPath "C$\Windows\Temp" | Join-Path -ChildPath $MSI)
     Invoke-Command -ComputerName $($_.Name) -ArgumentList $MSI -ScriptBlock {
-        Start-Process -FilePath (Join-Path -Path "C:" -ChildPath "Windows" | Join-Path -ChildPath "Temp" | Join-Path -ChildPath $using:MSI) -Wait -argumentlist '/quiet' 
+        Start-Process -FilePath $using:ImportedPath -Wait -argumentlist '/quiet' 
     }
 }
